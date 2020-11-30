a1bd88e7fa8049507af4706f0e38df3ab5d66621
michaelvaltn <michaelvalentine140@gmail.com>
2020-11-30 19:46:11 +0700

    Update 30/11/20
    Add+Styled the footer
    refined overall style
    scroll button more smooth and faster

--- js/scroll.js
+++ js/scroll.js
@@ -1,14 +1,18 @@
-/*$(function() {
-  $('a[href*=#]').on('click', function(e) {
-    e.preventDefault();
-  });
-}); */
+
 var scrolled = 0;
 $(document).ready(function() {
    $("#downClick").on("click" ,function() {
        // I assume scrolled is the height of the element you want to scroll to
-       scrolled = scrolled + 700;
-       $('body,html').animate({ scrollTop: scrolled }, 600);
+       $('body,html').animate({ scrollTop: scrolled }, 800);
+       scrolled = scrolled + 900;
        scrolled = 0;
    });
 });
+/*For scrolldown button
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});*/
+
