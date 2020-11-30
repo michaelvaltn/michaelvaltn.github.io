22b690c7f461292f559e35cd74bd9f9c2fdf5b1f
michaelvaltn <michaelvalentine140@gmail.com>
2020-11-30 20:02:53 +0700

    js

--- js/scroll.js
+++ js/scroll.js
@@ -1,18 +1,19 @@
-
 var scrolled = 0;
 $(document).ready(function() {
    $("#downClick").on("click" ,function() {
        // I assume scrolled is the height of the element you want to scroll to
-       $('body,html').animate({ scrollTop: scrolled }, 800);
-       scrolled = scrolled + 900;
+       scrolled = scrolled + 750;
+       $('body,html').animate({ scrollTop: scrolled }, 700);
        scrolled = 0;
    });
 });
-/*For scrolldown button
-$(function() {
-  $('a[href*=#]').on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-  });
-});*/
 
+var scrolled = 0;
+$(document).ready(function() {
+   $("#contact-button").on("click" ,function() {
+       // I assume scrolled is the height of the element you want to scroll to
+       scrolled = scrolled + 3000;
+       $('body,html').animate({ scrollTop: scrolled }, 700);
+       scrolled = 0;
+   });
+});
